8e018bc337897f9912782e27690ccb85c0973908
Mayowa Olajide <olajidemayorwa@gmail.com>
2025-08-03 12:01:01 +0100

    made some changes with the env properties

--- EmailService/src/main/java/com/mayorman/EmailService/listener/UserCreatedListener.java
+++ EmailService/src/main/java/com/mayorman/EmailService/listener/UserCreatedListener.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,8 @@ public class UserCreatedListener {
 
     @Autowired
     private JavaMailSender emailSender;
+    @Autowired
+    private Environment environment;
 
 
     // This annotation is the magic part. It tells Spring that this method
@@ -28,7 +31,7 @@ public class UserCreatedListener {
 
         // Create the welcome email
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("mayorman.cactus@gmail.com"); // This can be any "from" address you've verified
+        message.setFrom(environment.getProperty("spring.mail.from"));// This can be any "from" address you've verified
         message.setTo(employeeDto.getEmail());
         message.setSubject("Welcome to the Employee Management System!");
         message.setText("Hello " + employeeDto.getFirstName() + ",\n\nWelcome aboard! We are excited to have you.");
