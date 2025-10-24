37f0918f8861d90115d0b33f40a15b50f978a2ac
Mayorman07 <mayowa.olajide@flutterwavego.com>
2025-10-24 09:45:30 +0100

    adding dockerfiles to the config server and discovery service

--- EmailService/src/main/java/com/mayorman/EmailService/configuration/RabbitMQConfig.java
+++ EmailService/src/main/java/com/mayorman/EmailService/configuration/RabbitMQConfig.java
@@ -1,14 +1,17 @@
 package com.mayorman.EmailService.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class RabbitMQConfig {
 
     public static final String USER_EVENTS_EXCHANGE = "user-events-exchange";
